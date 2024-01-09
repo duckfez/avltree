31547c866eba9d91b250601c950b0e0e749673f0
Duane Waddle <duane.waddle@gmail.com>
2024-01-08 21:48:23 -0600

    restructured

--- avltree.h
+++ avltree.h
@@ -3,10 +3,33 @@
 
 #pragma once
 
-#include <iostream>
-#include <sstream>
-#include <functional>
 #include "fmt/core.h"
 #include "fmt/format.h"
+#include <functional>
+#include <iostream>
+#include <sstream>
 
 // TODO: Reference additional headers your program requires here.
+
+using namespace std;
+
+class TreeItem : public std::enable_shared_from_this<TreeItem> {
+public:
+  int value;
+  shared_ptr<TreeItem> left;
+  shared_ptr<TreeItem> right;
+
+  TreeItem(int val);
+  void Insert(int val);
+  void Visit(std::function<void(shared_ptr<TreeItem const>)> funky) const;
+};
+
+class Tree {
+  shared_ptr<TreeItem> root;
+
+public:
+  Tree();
+  void Insert(int value);
+  std::string Dotify(void);
+  std::string List(void);
+};
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "avltree.h"
+
+int main() {
+
+  std::vector<std::vector<int>> jimmy{// {9, 11, 17, 29, 4},
+                                      // {9, 11, 17, 29, 7},
+                                      {9, 3, 19, 20, 21, 22, 23, 24}};
+
+  try {
+    for (auto itemlist : jimmy) {
+      auto x = Tree();
+
+      for (auto var : itemlist) {
+        x.Insert(var);
+      }
+      std::cout << x.Dotify();
+    }
+
+  } catch (const std::invalid_argument &e) {
+    fmt::println("exception: {}", e.what());
+  }
+
+  return -1;
+}
